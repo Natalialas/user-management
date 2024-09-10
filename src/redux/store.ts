75feaeb1e434ecd7de0dfe1fba1c07da2fdf2530
Natalia Laszczkowska <natalia.laszczkowskax@gmail.com>
2024-09-10 12:15:42 +0200

    Add basic structure of files and remove unnecessary files

---
+++ src/redux/store.ts
@@ -0,0 +1,10 @@
+import { createStore, combineReducers } from 'redux';
+
+
+const rootReducer = combineReducers({
+
+});
+
+export type AppState = ReturnType<typeof rootReducer>;
+
+export const store = createStore(rootReducer);
